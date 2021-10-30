73660b5f1cde0952f4284fe97d5b0aea3e4ee252
dcs <553537528@qq.com>
2021-10-30 15:47:44 +0800

    Update redis.config.js

--- worker/redis.config.js
+++ worker/redis.config.js
@@ -2,7 +2,7 @@ const config = {
     port: 6388, // Redis port
     host: "47.106.21.200", // Redis host
     // family: 4, // 4 (IPv4) or 6 (IPv6)
-    password: "5GCampus",
+    password: "",
     connectTimeout: 10000,
     db: 0,
 }
